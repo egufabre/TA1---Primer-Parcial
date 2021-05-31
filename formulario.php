470e8bc69f2ebd1faba88f0b4abf934cd7949d5b
Eduardo Fabre <egufabre@hotmail.com>
2021-05-31 20:03:22 -0300

    se crea funcion para guardar informacion en la base de datos

--- formulario.php
+++ formulario.php
@@ -19,6 +19,14 @@
 </body>
 </html>
 <?php
+
+public $nombre;
+public $apellido;
+public $telefono
+public $fecha;
+public $edad;
+public $email;
+
 public static function insertarPersona($nombre,$apellido,$telefono,$fecha,$edad,$email){
     $p = new PersonaModelo();
     $p -> nombre = $nombre
@@ -29,4 +37,15 @@ public static function insertarPersona($nombre,$apellido,$telefono,$fecha,$edad,
     $p -> email = $email
     $p -> return guardar();
     
+}
+public function guardar() {
+    $sql = "INSERT INTO persona(nombre,apellido,telefono,fecha,edad,email) VALUES (
+        '{$nombre -> nombre}'
+        '{$apellido -> apellido}'
+        '{$telefono -> telefono}'
+        '{$fecha -> fecha}'
+        '{$edad -> edad}'
+        '{$email -> email}'
+        )";
+         "
 }
