a115f1daab8d3e5def339858ecf330dfac06e3a4
Eduardo Fabre <egufabre@hotmail.com>
2021-05-31 20:28:48 -0300

    se divide en formularios y formulario de inicio.php

--- formulario.php
+++ formulario.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body> 
-    <form action="" method="post">
-        Nombre: <input type="text" name="nombre" id="nombre"> <br>
-        Apellido: <input type="text" name="apellido" id="apellido"> <br>
-        Telefono: <input type="text" name="telefono" id="telefono"> <br>
-        Fecha: <input type="date" name="fecha" id= "fecha"> <br>
-        Edad: <input type="number" name="edad" id="edad"> <br>
-        Email: <input type="email" name="email" id="email"> <br>
-        <input type="submit" value="Enviar">
-    </form>
-</body>
-</html>
 <?php
 
 public $nombre;
