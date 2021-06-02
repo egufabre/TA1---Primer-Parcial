fb7952e7bcb3a131f881c341e36164bd1fbafa9c
Eduardo Fabre <egufabre@hotmail.com>
2021-06-01 23:16:13 -0300

    se crearon las validaciones, faltó colocarlas en el método

--- formulario.php
+++ formulario.php
@@ -12,26 +12,35 @@ public static function insertarPersona($nombre,$apellido,$telefono,$fecha,$edad,
     $edad = $_POST['edad'];
     $email = $_POST['email'];
     guardar();
-    
 }
+
 public function guardar() {
     $sql = "INSERT INTO persona(nombre,apellido,telefono,fecha,edad,email) VALUES (
         '$nombre', '$apellido', '$telefono', '$fecha', '$edad', '$email')";
 mysqli_query($base,$sql);
 }
+
 public function error() {
     if($_SERVER['REQUEST_METHOD'] === 'GET'){
         header("HTTP/1.1 500 Internal server error");
         die("Internal server error");
-        }
     }
+}
+
 public function agregadoCorrectamente() {
     echo "Persona Agregada correctamente"
 }
 
-    function validarCampos($formulario){
-        if($formulario['nombre'] === "" || $formulario['apellido'] === "" || $telefono['telefono'] || $fecha['fecha'] || $formulario['edad'] === "" || $formulario['email'] === ""){           
-            return error();
-        }
-        return agregadoCorrectamente();
+public function validarCampos($formulario){
+    if($formulario['nombre'] === "" || $formulario['apellido'] === "" || $telefono['telefono'] || $fecha['fecha'] || $formulario['edad'] === "" || $formulario['email'] === ""){           
+        return error();
     }
+    return agregadoCorrectamente();
+}
+public function validarNombreYApellido($formulario){
+    if($formulario['nombre'] == $formulario['apellido']){
+        return error();
+    }
+    return agregadoCorrectamente();
+}
+
